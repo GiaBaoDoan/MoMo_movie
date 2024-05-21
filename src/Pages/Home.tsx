ba2da978a66b50837e37f13ec58bf4014a041255
Đoàn Gia Bảo <giabaod345@gmail.com>
2024-05-21 20:22:54 +0700

    up

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,9 @@
 import { HomTemPlate } from "components/templates";
-import { PATH } from "constant";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+// import { PATH } from "constant";
+// import { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
 const Home = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   // useEffect(() => {
   //   if (!JSON.parse(localStorage.getItem("USER"))) {
   //     navigate(PATH.login);
