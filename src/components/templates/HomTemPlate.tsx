cc9c0e28b493ceff13c4b159055eb514d88bba33
Đoàn Gia Bảo <giabaod345@gmail.com>
2024-05-30 22:19:23 +0700

    up

--- src/components/templates/HomTemPlate.tsx
+++ src/components/templates/HomTemPlate.tsx
@@ -133,7 +133,7 @@ export const HomTemPlate = () => {
       />
       {/* banner */}
       <BannerMovie />
-      <section className="bg-pinkTheme/20">
+      <section className="bg-pink-100">
         <div className="py-24 max-lg:py-12 flex lg:space-x-5 justify-between max-lg:space-y-7 items-center max-lg:items-start max-lg:flex-col w-[90%] mx-auto">
           <div>
             <h2 className="text-4xl text-pinkTheme max-sm:text-2xl font-bold">
@@ -391,7 +391,7 @@ export const HomTemPlate = () => {
         </div>
       </section>
       {/* blog film ảnh */}
-      <section className="bg-pinkTheme/20 py-24 max-lg:py-12">
+      <section className="bg-pink-100 py-24 max-lg:py-12">
         <div className="text-center w-[90%] mx-auto">
           <h3 className="font-bold  text-4xl max-sm:text-2xl text-pinkTheme">
             Blog phim ảnh
@@ -421,104 +421,106 @@ export const HomTemPlate = () => {
         </div>
       </section>
       {/*  */}
-      <section className="py-24 max-lg:py-12 bg-white w-[90%] mx-auto">
-        <h3 className="text-4xl max-sm:text-2xl text-center font-bold text-pinkTheme">
-          Đặt mua vé xem phim trên MoMo
-        </h3>
-        <div className="flex justify-center items-center">
-          <p className="text-xl w-[85%] max-sm:text-lg max-lg:w-full mt-5 font-500">
-            Việc đặt vé xem phim chưa bao giờ đơn giản và dễ dàng như thế, chỉ
-            với vài thao tác trên màn hình điện thoại bạn đã có thể đặt vé xem
-            bộ phim mình yêu thích mà không phải xếp hàng tại rạp. Ví MoMo đã
-            liên kết tất cả các rạp chiếu phim lớn trên cả nước, rạp nào bạn
-            thích Ví MoMo cũng có. Giá vé xem phim luôn ở mức giá cực kì tốt là
-            một “đặc sản” với những ưu đãi độc quyền mà chỉ có ở “Mua Vé Xem
-            Phim” của Ví MoMo. <br /> <br /> Nay buổi xem{" "}
-            <span className="font-bold text-pinkTheme">phim chiếu rạp</span> của
-            bạn sẽ càng tuyệt vời hơn với tính năng mua bắp nước trước khi đến
-            rạp, bạn sẽ đa dạng hoá các món bắp hay thức uống mà bạn yêu thích,
-            tạo bất ngờ cho bạn đồng hành bằng những combo cực chất lượng đến từ
-            các{" "}
-            <span className="text-pinkTheme font-bold">rạp chiếu phim.</span>
-            <p className="mt-5 font-bold text-2xl max-sm:text-xl text-pinkTheme">
-              Giá vé rạp chiếu phim (Phim 2D - dành cho người lớn)
+      <section className="py-24 max-lg:py-12 bg-white">
+        <div className=" mx-auto w-[90%]">
+          <h3 className="text-4xl max-sm:text-2xl text-center font-bold text-pinkTheme">
+            Đặt mua vé xem phim trên MoMo
+          </h3>
+          <div className="flex justify-center items-center">
+            <p className="text-xl w-[85%] max-sm:text-lg max-lg:w-full mt-5 font-500">
+              Việc đặt vé xem phim chưa bao giờ đơn giản và dễ dàng như thế, chỉ
+              với vài thao tác trên màn hình điện thoại bạn đã có thể đặt vé xem
+              bộ phim mình yêu thích mà không phải xếp hàng tại rạp. Ví MoMo đã
+              liên kết tất cả các rạp chiếu phim lớn trên cả nước, rạp nào bạn
+              thích Ví MoMo cũng có. Giá vé xem phim luôn ở mức giá cực kì tốt
+              là một “đặc sản” với những ưu đãi độc quyền mà chỉ có ở “Mua Vé
+              Xem Phim” của Ví MoMo. <br /> <br /> Nay buổi xem{" "}
+              <span className="font-bold text-pinkTheme">phim chiếu rạp</span>{" "}
+              của bạn sẽ càng tuyệt vời hơn với tính năng mua bắp nước trước khi
+              đến rạp, bạn sẽ đa dạng hoá các món bắp hay thức uống mà bạn yêu
+              thích, tạo bất ngờ cho bạn đồng hành bằng những combo cực chất
+              lượng đến từ các{" "}
+              <span className="text-pinkTheme font-bold">rạp chiếu phim.</span>
+              <p className="mt-5 font-bold text-2xl max-sm:text-xl text-pinkTheme">
+                Giá vé rạp chiếu phim (Phim 2D - dành cho người lớn)
+              </p>
+              <div className="max-sm:overflow-scroll">
+                <table className="border w-full mt-5">
+                  <thead>
+                    <tr className="text-pinkTheme text-lg">
+                      <th>Rạp</th>
+                      <th>Thứ 2</th>
+                      <th>Thứ 3</th>
+                      <th>Thứ 4</th>
+                      <th>Thứ 5</th>
+                      <th>Thứ 6</th>
+                      <th>Thứ 7</th>
+                      <th>Chủ nhật</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="text-center">
+                      <td className="rap text-lg text-pinkTheme">Galaxys</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                    </tr>
+                    <tr className="text-center">
+                      <td className="rap text-pinkTheme">CGV</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                    </tr>
+                    <tr className="text-center">
+                      <td className="rap text-pinkTheme">BHD</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                    </tr>
+                    <tr className="text-center">
+                      <td className="rap text-pinkTheme">Lotte Cinema</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                    </tr>
+                    <tr className="text-center">
+                      <td className="rap text-pinkTheme">Cinestar</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                      <td>70K - 85K</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <p className="text-xl max-sm:text-lg mt-5">
+                Hy vọng rằng những trải nghiệm đặt vé xem phim, tra cứu lịch
+                chiếu phim nhanh, tiện lợi với mức giá tốt sẽ là món ăn tinh
+                thần tuyệt vời dành cho người dùng của Ví MoMo. Đặt vé xem phim
+                online chưa bao giờ dễ dàng và chuyên nghiệp như thế khi có Ví
+                MoMo đồng hành cùng bạn!
+              </p>
             </p>
-            <div className="max-sm:overflow-scroll">
-              {/* <table className="border w-full mt-5">
-                <thead>
-                  <tr className="text-pinkTheme text-lg">
-                    <th>Rạp</th>
-                    <th>Thứ 2</th>
-                    <th>Thứ 3</th>
-                    <th>Thứ 4</th>
-                    <th>Thứ 5</th>
-                    <th>Thứ 6</th>
-                    <th>Thứ 7</th>
-                    <th>Chủ nhật</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr className="text-center">
-                    <td className="rap text-lg text-pinkTheme">Galaxys</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                  </tr>
-                  <tr className="text-center">
-                    <td className="rap text-pinkTheme">CGV</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                  </tr>
-                  <tr className="text-center">
-                    <td className="rap text-pinkTheme">BHD</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                  </tr>
-                  <tr className="text-center">
-                    <td className="rap text-pinkTheme">Lotte Cinema</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                  </tr>
-                  <tr className="text-center">
-                    <td className="rap text-pinkTheme">Cinestar</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                    <td>70K - 85K</td>
-                  </tr>
-                </tbody>
-              </table> */}
-            </div>
-            <p className="text-xl max-sm:text-lg mt-5">
-              Hy vọng rằng những trải nghiệm đặt vé xem phim, tra cứu lịch chiếu
-              phim nhanh, tiện lợi với mức giá tốt sẽ là món ăn tinh thần tuyệt
-              vời dành cho người dùng của Ví MoMo. Đặt vé xem phim online chưa
-              bao giờ dễ dàng và chuyên nghiệp như thế khi có Ví MoMo đồng hành
-              cùng bạn!
-            </p>
-          </p>
+          </div>
         </div>
       </section>
     </Homtemplate>
